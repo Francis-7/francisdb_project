326562fc56ddeb03d596e715e4ff733847b6af75
Francis Awoh <realmabove02@gmail.com>
2024-12-21 04:46:02 -0800

    correction in settings

--- web_project/settings.py
+++ web_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listingsApp.apps.istingsappConfig',
+    'listingsApp.apps.ListingsappConfig',
     'quizApp.apps.QuizappConfig'
 ]
 
