c7c9f4ca4eb482822ea748f09781a17e2ea8efa8
Francis Awoh <realmabove02@gmail.com>
2024-12-21 10:47:24 -0800

    created views

--- listingsApp/views.py
+++ listingsApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+def product_list(request):
+  categories = Category.objects.all()
+  products = Product.objects.all()
+
+  return render(request, 'product/list.html', {
+    'categories' : categories,
+    'products' : products
+  })
