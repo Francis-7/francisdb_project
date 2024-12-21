e3ecc8b6f71be241597d542551c4e07a05eeeccc
Francis Awoh <realmabove02@gmail.com>
2024-12-21 09:59:03 -0800

    added settings for models

--- listingsApp/admin.py
+++ listingsApp/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Category
-admin.site.register(Category)
+# admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'slug')
+  prepopulated_fields = {'slug': ('name',)}
--- listingsApp/models.py
+++ listingsApp/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     ordering = ('-name',)
     verbose_name_plural = 'categories'
 
+  def __str__(self):
+    return self.name
+
 class Product(models.Model):
   category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
   name = models.CharField(max_length=100, unique=True)
@@ -21,3 +24,6 @@ class Product(models.Model):
 
   class Meta:
     ordering = ('shu',)
+
+  def __str__(self):
+    return self.nam
